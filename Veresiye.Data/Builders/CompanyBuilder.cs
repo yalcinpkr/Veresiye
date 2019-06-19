f7d3ef3b5ae4f80d457f3b9249f68ba1e22a96d0
yalcinpkr <yalcin_peker@hotmail.com>
2019-06-19 10:00:21 +0300

    Fluent-api Eklendi

---
+++ Veresiye.Data/Builders/CompanyBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veresiye.Model;
+
+namespace Veresiye.Data.Builders
+{
+   public class CompanyBuilder
+    {
+        public CompanyBuilder(EntityTypeConfiguration<Company> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Name).IsRequired().HasMaxLength(100);
+            builder.Property(a => a.Phone).IsRequired().HasMaxLength(100);
+            builder.Property(a => a.Region).HasMaxLength(100);
+            builder.Property(a => a.City).HasMaxLength(100);
+        }
+    }
+}
